49dbab60fd267d0a8887c7902e996de0ed7dc15c
DussedD <dasorokin123@gmail.com>
2022-12-24 18:26:30 +0300

    Added RevRecord, RevInputScreen and FlightRevenue classes

--- Transport_Company/tc.cpp
+++ Transport_Company/tc.cpp
@@ -18,16 +18,149 @@ char getaChar() // получение символа
 
 
 
+
+
+//методы класса FlightRevenue//
+FlightRevenue::FlightRevenue(int an) : FlightNo(an) //конструктор
+{ //Алгоритм fill() помещает копию значения value (у нас это 0)
+//в каждый элемент диапазона, ограниченного парой итераторов [first,last).
+//Т.е. в конструкторе просто инициализируем массив значениями 0.
+    fill(&months[0], &months[12], 0);
+}
+//---------------------------------------------------------
+void FlightRevenue::setRev(int m, float am) // сеттер доход за месяц m, сумма - am
+{
+    months[m] = am; // привязываем оплату к месяцу
+}
+//---------------------------------------------------------
+int FlightRevenue::getFlightNo() // геттер запрос номера рейса
+{
+    return FlightNo;
+}
+//---------------------------------------------------------
+float FlightRevenue::getRevNo(int month) //Геттер запрос дохода за месяц month
+{
+    return months[month];
+}
+//---------------------------------------------------------
+float FlightRevenue::getSumOfRev() // cумма дохода в строке
+{ //По умолчанию алгоритм accumulate() суммирует элементы.
+ //Нужно указать точку старта, конечную точку и значение от которого начинаем прибавлять.
+ //Чаще всего это ноль, но может быть и результат других вычислений.
+    return accumulate(&months[0], &months[12], 0);
+}
+
+
+
+//методы класса RevRecord//
+RevRecord::~RevRecord() // деструктор
+{ // удалить строки с платежами,
+// удалить указатели из множества.
+    while (!setPtrsRR.empty())
+    {
+        iter = setPtrsRR.begin();
+        delete* iter;
+        setPtrsRR.erase(iter);
+    }
+}
+
+void RevRecord::insertRev(int FlightNo, int month, float amount)
+{
+    iter = setPtrsRR.begin(); // Инициализация итератора
+    while (iter != setPtrsRR.end()) // условие выхода
+    { // если текущий объект совпадает с созданным для поиска,
+        if (FlightNo == (*iter)->getFlightNo())
+    { // заносим доход в список
+            (*iter)->setRev(month, amount);
+            return;
+        }
+        else
+            iter++;
+    } // если не нашли строку - создаем новую
+    FlightRevenue* ptrRow = new FlightRevenue(FlightNo);
+    ptrRow->setRev(month, amount); // заносим в нее платеж
+    setPtrsRR.push_back(ptrRow); // заносим строку в вектор
+}
+
+void RevRecord::display() // отобразить все строки с доходами
+{
+    cout << "\nNumber\tJan Feb Mar Apr May June July Aug Sept Oct Nov Dec\n" << endl
+        << "------------------------------------------------------------------\n" << endl;
+    if (setPtrsRR.empty())
+        cout << "***No income***\n" << endl;
+    else
+    {
+        iter = setPtrsRR.begin(); //итератор на список с указателями на объекты FlightRevenue
+        while (iter != setPtrsRR.end())
+        {
+            cout << (*iter)->getFlightNo() << '\t'; // вывести номер рейса
+            for (int j = 0; j < 12; j++) // вывести доходы по месяцам
+            {
+                if (((*iter)->getRevNo(j)) == 0)
+                    cout << " 0 ";
+                else
+                    cout << (*iter)->getRevNo(j) << " ";
+            }
+            cout << endl;
+            iter++;
+        }
+        cout << endl;
+        cout << endl;
+    }
+}
+
+float RevRecord::getSumOfRev() // сумма всех платежей
+{
+    float sumRevs = 0.0;
+    iter = setPtrsRR.begin();
+    while (iter != setPtrsRR.end())
+    { // плюсуем суммы всех доходов водителей за все время
+        sumRevs += (*iter)->getSumOfRev();
+        iter++;
+    }
+    return sumRevs;
+}
+
+
+
+
+//методы класса RevInputScreen//
+void RevInputScreen::setRev()
+{
+    cout << "Enter the driver's name: ";
+    getaLine(revName);
+    // получить номер рейса по имени водителя
+    FlightNo = ptrDriversList->getFlightNo(revName);
+    if (FlightNo > 0) // если имя найдено, и такой водитель существует -
+    { // получить сумму платежа
+        cout << "Enter the amount of income (800.50): " << endl; // Введите сумму дохода (800.50)
+        cin >> revPaid; // вводим цену
+        cin.ignore(80, '\n');
+        cout << "Enter the payment month number (1-12): " << endl; // Введите номер месяца оплаты (1-12)
+        cin >> month;
+        cin.ignore(80, '\n');
+        month--; // (внутренняя нумерация 0-11)
+         // вставляем сумму в запись о доходе
+        ptrRevRecord->insertRev(FlightNo, month, revPaid);
+    }
+    else
+        cout << "There is no such driver.\n" << endl; //Такого водителя нет
+}
+
+
+
 //методы класса UserInterface//
 UserInterface::UserInterface()
 {
     ptrDriversList = new DriversList;
+    ptrRevRecord = new RevRecord;
 }
 
 
 UserInterface::~UserInterface()
 {
     delete ptrDriversList;
+    delete ptrRevRecord;
 }
 
 
@@ -36,8 +169,10 @@ void UserInterface::interact()
     while (true)
     {
             cout << "\n Click to add a driver 'a', \n"      //Нажмите для добавления водителя
-                << " Click to withdraw drivers 'd', \n"		//Нажмите для вывода водителей
-                << " Click to exit the program 'q'\n";      //Нажмите для выхода из программы
+                 << " Click to record the driver's income 'b', \n"  //Нажмите для записи дохода водителя
+                 << " Click to withdraw drivers 'd', \n"		//Нажмите для вывода водителей
+                 << " Click to withdraw revenue 'e', \n"          //Нажмите для вывода доходов
+                 << " Click to exit the program 'q'\n";      //Нажмите для выхода из программы
             ch = getaChar();
             switch (ch)
             {
@@ -45,7 +180,13 @@ void UserInterface::interact()
                 ptrDriversInputScreen->setDriver();
                 delete ptrDriversInputScreen;
                 break;
+            case 'b': ptrRevInputScreen = new RevInputScreen(ptrDriversList, ptrRevRecord);
+                            ptrRevInputScreen->setRev();
+                            delete ptrRevInputScreen;
+                            break;
             case 'd': ptrDriversList->display();
+                break;
+            case 'e': ptrRevRecord->display();
                 break;
             case 'q':
                 cout << "The program is completed"; // Программа завершена
@@ -56,3 +197,4 @@ void UserInterface::interact()
             } // конец switch
     } // конец while
 } // конец interact()
+
--- Transport_Company/tc.h
+++ Transport_Company/tc.h
@@ -1,8 +1,11 @@
 //tc.h
 #ifndef TC_H
 #define TC_H
+
 #include <iostream>
 #include <list>
+#include <vector>
+#include <numeric> //для accumulate()
 #include "Drivers.h"
 #include "DriversList.h"
 #include "DriversInputScreen.h"
@@ -10,6 +13,63 @@ using namespace std;
 // глобальные методы //
 void getaLine(string& inStr); // получение строки текста
 char getaChar(); // получение символа
+
+
+////////////////////класс FlightRevenue//////////////////////////
+//класс, хранящий одну табличную строку доходов с рейса
+// одна строка таблицы прибыли: рейс и 12 доходов по месяцам
+class FlightRevenue
+{
+private:
+    int FlightNo; // рейсы, за которые уплачено
+    float months[12]; // месяцы
+public:
+    FlightRevenue(int); // конструктор с одним параметром
+    void setRev(int, float); // добавить пллату за месяц
+    //сумма платежей из одной строки (прибыль с одного водителя за все месяцы)
+    float getSumOfRev();
+    int getFlightNo(); //Запрос номера рейса
+    float getRevNo(int); //Запрос дохода за месяц int
+};
+
+
+//////////////////// класс RevRecord ///////////////////////
+//класс RevRecord. Он хранит непосредственно записи доходов.
+//С ним будет взаимодействовать экран добавления дохода.
+class RevRecord
+{
+private:
+    list <FlightRevenue*> setPtrsRR; // указатели на объекты FlightRevenue (по одному на водителя)
+    list <FlightRevenue*>::iterator iter;
+public:
+    ~RevRecord();
+    void insertRev(int, int, float); // добавить плату
+    void display(); // отобразить все строки с платами
+    float getSumOfRev(); // подсчитать сумму всех платежей всех водителей
+};
+
+
+
+////////////////////класс RevInputScreen //////////////////
+//Экран для добавления платы
+class RevInputScreen
+{
+private:
+    DriversList* ptrDriversList; // список водителей
+    RevRecord* ptrRevRecord; // список записей об оплате
+    string revName; // имя водителя, который приносит прибыль
+    float revPaid; // плата
+    int month; // за месяц
+    int FlightNo; // по рейсам
+public:
+    RevInputScreen(DriversList* ptrDL, RevRecord* ptrRR) : ptrDriversList(ptrDL),
+        ptrRevRecord(ptrRR)
+    {
+        /*тут пусто*/
+    }
+    void setRev(); // добавить доход с одного водителя за месяц
+};
+
 
 
 
@@ -21,6 +81,9 @@ class UserInterface
 private:
     DriversList* ptrDriversList;
     DriversInputScreen* ptrDriversInputScreen;
+    RevRecord* ptrRevRecord;
+    RevInputScreen* ptrRevInputScreen;
+
     char ch;
 public:
     UserInterface();
@@ -28,3 +91,4 @@ public:
     void interact();
 };
 #endif // TC_H
+
